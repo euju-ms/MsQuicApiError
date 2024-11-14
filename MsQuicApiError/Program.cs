bf805d6ccf41f125b646acb3c4ed79a05381bad7
Eui Jung <euju@microsoft.com>
2024-11-14 13:40:09 -0800

    test app

---
+++ MsQuicApiError/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+HttpClient httpClient = new HttpClient();
+string st = httpClient.ToString() ?? string.Empty;
+
+string uri = "Https://www.bing.com";
+HttpResponseMessage response = await httpClient.GetAsync(uri).ConfigureAwait(true);
+
+Console.WriteLine($"Hello, World! {st} {response}");
